492bc0935905819065b4d72a4ed4e3f637e5f5a0
Mengqi Xie <mengqi.xie@duke.edu>
2025-03-17 10:26:30 -0400

    Update tabular_gan.py
    line 194 cond = cond

--- src/synthcity/plugins/core/models/tabular_gan.py
+++ src/synthcity/plugins/core/models/tabular_gan.py
@@ -191,8 +191,9 @@ class TabularGAN(torch.nn.Module):
             if len(cond.shape) == 1:
                 cond = cond.reshape(-1, 1)
 
-            self.cond_encoder = OneHotEncoder(handle_unknown="ignore").fit(cond)
-            cond = self.cond_encoder.transform(cond).toarray()
+            #self.cond_encoder = OneHotEncoder(handle_unknown="ignore").fit(cond)
+            #cond = self.cond_encoder.transform(cond).toarray()
+            cond = cond
 
             n_units_conditional = cond.shape[-1]
 
